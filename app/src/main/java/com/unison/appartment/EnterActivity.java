e3a9ad986b3e3c5eafa59ec3c5ab3d69fe8dbf0e
MarcoCadei <m.cadei010@studenti.unibs.it>
2019-04-13 21:19:04 +0200

    Realizzata activity di accesso all'applicazione

--- app/src/main/java/com/unison/appartment/EnterActivity.java
+++ app/src/main/java/com/unison/appartment/EnterActivity.java
@@ -1,13 +1,14 @@
 package com.unison.appartment;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class EnterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_enter);
     }
 }
