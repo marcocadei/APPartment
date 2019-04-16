e69fc5dbecbf0d910e6590b45549be627c62bb7f
MarcoCadei <m.cadei010@studenti.unibs.it>
2019-04-16 17:44:11 +0200

    Aggiunti listener ai bottoni nella create home activity

--- app/src/main/java/com/unison/appartment/CreateHomeActivity.java
+++ app/src/main/java/com/unison/appartment/CreateHomeActivity.java
@@ -2,7 +2,11 @@ package com.unison.appartment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CreateHomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class CreateHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_home);
+
+        FloatingActionButton floatNext = findViewById(R.id.activity_create_home_float_next);
+        floatNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(CreateHomeActivity.this, CreateMemberActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
