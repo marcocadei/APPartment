c036bbe074b7cd5d6ee73b831fc1d513384df0e8
AndreaPitossi <isdreavus95@gmail.com>
2019-04-16 12:20:31 +0200

    Implementata activity per la creazione della casa

--- app/src/main/java/com/unison/appartment/EnterActivity.java
+++ app/src/main/java/com/unison/appartment/EnterActivity.java
@@ -2,7 +2,10 @@ package com.unison.appartment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EnterActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class EnterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter);
+
+        Button btnRegister = findViewById(R.id.activity_enter_btn_register);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(EnterActivity.this, CreateHomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
