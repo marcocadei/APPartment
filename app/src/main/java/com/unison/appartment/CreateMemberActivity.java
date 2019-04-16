2a7143327f65d782484d791389ec3c7ea0fe0833
MarcoCadei <m.cadei010@studenti.unibs.it>
2019-04-16 18:20:07 +0200

    Aggiunto listenere al bottone new member della create member activity impostando come flag nell'intent che non si aprano più activity, ma solo una

--- app/src/main/java/com/unison/appartment/CreateMemberActivity.java
+++ app/src/main/java/com/unison/appartment/CreateMemberActivity.java
@@ -2,7 +2,11 @@ package com.unison.appartment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CreateMemberActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class CreateMemberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_member);
+
+        FloatingActionButton floatNewMember = findViewById(R.id.activity_create_member_float_new_member);
+        floatNewMember.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(CreateMemberActivity.this, CreateMemberActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
     }
 }
