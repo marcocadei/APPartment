314f9eb6c9fdae9cb58b65cefb23a4f61dcefb07
MarcoCadei <m.cadei010@studenti.unibs.it>
2019-04-20 17:48:01 +0200

    Rinominato id errato del fab nell'activity signin

--- app/src/main/java/com/unison/appartment/SignInActivity.java
+++ app/src/main/java/com/unison/appartment/SignInActivity.java
@@ -3,6 +3,7 @@ package com.unison.appartment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Button btnSignIn = findViewById(R.id.fl)
     }
 }
