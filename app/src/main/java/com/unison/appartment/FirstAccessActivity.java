6bb88b985b8b818f82aa7bee0d1bb8abcb7cb21d
gianluca150 <pigic116@gmail.com>
2019-04-23 11:31:38 +0200

    Creata FirstAccessActivity

---
+++ app/src/main/java/com/unison/appartment/FirstAccessActivity.java
@@ -0,0 +1,14 @@
+package com.unison.appartment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstAccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_access);
+    }
+}
