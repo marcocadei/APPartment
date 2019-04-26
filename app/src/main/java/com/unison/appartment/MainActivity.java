c4e3e827e22449afbbf6f2ff414e29f4583ed034
MarcoCadei <m.cadei010@studenti.unibs.it>
2019-04-26 18:32:11 +0200

    Migliorata struttura activity main e aggiunti commenti

--- app/src/main/java/com/unison/appartment/MainActivity.java
+++ app/src/main/java/com/unison/appartment/MainActivity.java
@@ -3,11 +3,13 @@ package com.unison.appartment;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -17,13 +19,14 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
 
     private int selectedBottomNavigationMenuItemId;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Supporto per la toolbar
-        final Toolbar toolbar = findViewById(R.id.activity_main_toolbar);
+        toolbar = findViewById(R.id.activity_main_toolbar);
         setSupportActionBar(toolbar);
 
         // Le voci della bottom navigation sono un menu
@@ -32,13 +35,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigation = findViewById(R.id.activity_main_bottom_navigation);
         selectedBottomNavigationMenuItemId = bottomNavigation.getSelectedItemId();
         final MenuItem selectedBottomNavigationMenuItem = bottomNavigation.getMenu().findItem(selectedBottomNavigationMenuItemId);
-        toolbar.setTitle(selectedBottomNavigationMenuItem.getTitle());
-        toolbar.setLogo(selectedBottomNavigationMenuItem.getIcon());
-        // Aggiungo il fragment del bottone selezionato nella bottom navigation
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.activity_main_fragment_container, new MessagesFragment());
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        ft.commit();
+        updateActivityContent(selectedBottomNavigationMenuItem);
 
         bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -46,14 +43,69 @@ public class MainActivity extends AppCompatActivity {
                 // In questo modo quando si seleziona la stessa sezione in cui si è già non viene
                 // ricaricato il fragment (semplicemente non viene fatto nulla).
                 if (menuItem.getItemId() != selectedBottomNavigationMenuItemId) {
-                    // TODO Switch Fragment
-                    toolbar.setTitle(menuItem.getTitle());
-                    toolbar.setLogo(menuItem.getIcon());
+                    updateActivityContent(menuItem);
                 }
                 selectedBottomNavigationMenuItemId = menuItem.getItemId();
                 return true;
             }
         });
+    }
+
+    /**
+     * Aggiorna il contenuto dell'activity, ovvero la toolbar e il fragment centrale,
+     * in base alla voce selezionata nella bottom navigation
+     * @param menuItem voce del menù selezionata nella bottom navigation
+     */
+    private void updateActivityContent(MenuItem menuItem) {
+        toolbar.setTitle(menuItem.getTitle());
+        toolbar.setLogo(menuItem.getIcon());
+        switchToFragment(menuItem.getItemId());
+    }
+
+    /**
+     * Rimpiazzo il fragment corrente con quello corretto in base alla voce selezionata nella
+     * bottom navigation
+     * @param menuItemId l'id della voce del menù selezionata nella bottom navigation
+     */
+    private void switchToFragment(int menuItemId){
+        switch (menuItemId) {
+            case R.id.activity_main_bottom_navigation_messages:
+                switchToFragment(MessagesFragment.class);
+                break;
+            case R.id.activity_main_bottom_navigation_family:
+                // TODO aggiungere fragment family
+                break;
+            case R.id.activity_main_bottom_navigation_todo:
+                // TODO aggiungere fragment todo
+                break;
+            case R.id.activity_main_bottom_navigation_done:
+                // TODO aggiungere fragment done
+                break;
+            case R.id.activity_main_bottom_navigation_rewards:
+                // TODO aggiungere fragment rewards
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    /**
+     * Rimpiazzo il fragment corrente con quello corretto in base alla voce selezionata nella
+     * bottom navigation
+     * @param fragment la classe del fragment che andrà a sostituire quello corrente
+     */
+    private void switchToFragment(Class fragment) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        try {
+            ft.replace(R.id.activity_main_fragment_container, (Fragment) fragment.newInstance());
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+        // ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        ft.commit();
     }
 
     /**
