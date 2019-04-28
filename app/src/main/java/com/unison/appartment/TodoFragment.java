d572fd3dcfd4e73e0f4f466d854dea2279d6a960
MarcoCadei <m.cadei010@studenti.unibs.it>
2019-04-28 10:44:39 +0200

    Creato fragment padre che conterrà tutta la schermata mostrata per le attività ancora da svolgere

---
+++ app/src/main/java/com/unison/appartment/TodoFragment.java
@@ -0,0 +1,53 @@
+package com.unison.appartment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Fragment che rappresenta la lista delle attività da svolgere
+ */
+public class TodoFragment extends Fragment {
+
+    /**
+     * Costruttore vuoto obbligatorio che viene usato nella creazione del fragment
+     */
+    public TodoFragment() {
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static TodoFragment newInstance(String param1, String param2) {
+        TodoFragment fragment = new TodoFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_todo, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
