c72470b5547cb70efdb2a95c1dfc9b2a8769641e
MarcoCadei <m.cadei010@studenti.unibs.it>
2019-04-28 16:44:04 +0200

    Aggiunto date picker

--- app/src/main/java/com/unison/appartment/CreateTaskActivity.java
+++ app/src/main/java/com/unison/appartment/CreateTaskActivity.java
@@ -2,14 +2,23 @@ package com.unison.appartment;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class CreateTaskActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private EditText inputDeadline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +34,42 @@ public class CreateTaskActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        inputDeadline = findViewById(R.id.activity_create_task_input_deadline_value);
+        inputDeadline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+                Log.d("data_selezionata", "cliccato");
+            }
+        });
+    }
+
+    public void showDatePickerDialog() {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Uso la data corrente come quella di default nel date picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        // La data è stata impostata
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            Log.d("data_selezionata", year+"-"+month+"-"+dayOfMonth);
+        }
     }
 }
--- app/src/main/java/com/unison/appartment/TodoFragment.java
+++ app/src/main/java/com/unison/appartment/TodoFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
