932b88a8f1fdaee89d9de8f5af9ac6b137680ae0
gianluca150 <pigic116@gmail.com>
2019-05-09 11:08:06 +0200

    Modificata HomeListActivity (l'activity attualmente è ancora vuota)

--- app/src/main/java/com/unison/appartment/HomeListActivity.java
+++ app/src/main/java/com/unison/appartment/HomeListActivity.java
@@ -1,33 +1,55 @@
 package com.unison.appartment;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import com.unison.appartment.model.Home;
 
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class HomeListActivity extends AppCompatActivity {
+import java.util.LinkedList;
+import java.util.List;
+
+public class HomeListActivity extends ListActivity {
+
+    private ListView mListView;
+    private ArrayAdapter<Home> mAdapter;
+    private List<Home> mModel = new LinkedList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_list);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mListView = getListView();
+        mAdapter = new ArrayAdapter<Home>(this,
+                R.layout.activity_home_list_item,
+                mModel);
+        mListView.setAdapter(mAdapter);
+
+        FloatingActionButton floatNew = findViewById(R.id.activity_home_list_float_new);
+        floatNew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(HomeListActivity.this, CreateHomeActivity.class);
+                startActivity(i);
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton floatJoin = findViewById(R.id.activity_home_list_float_join);
+        floatJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeListActivity.this, JoinHomeActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 }
