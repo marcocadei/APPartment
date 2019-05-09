5998f7e934d99f49c3edf85e17f0906895848e08
gianluca150 <pigic116@gmail.com>
2019-05-09 10:30:28 +0200

    Aggiornata SignInActivity

--- app/src/main/java/com/unison/appartment/SignInActivity.java
+++ app/src/main/java/com/unison/appartment/SignInActivity.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -16,7 +15,6 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.SignInMethodQueryResult;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,11 +23,9 @@ import com.google.firebase.database.ValueEventListener;
 
 public class SignInActivity extends AppCompatActivity implements FirebaseErrorDialogFragment.FirebaseErrorDialogInterface {
 
-    EditText inputHomeName;
-    EditText inputUsername;
+    EditText inputEmail;
     EditText inputPassword;
-    TextInputLayout layoutHomeName;
-    TextInputLayout layoutUsername;
+    TextInputLayout layoutEmail;
     TextInputLayout layoutPassword;
 
     ProgressDialog progress;
@@ -39,11 +35,9 @@ public class SignInActivity extends AppCompatActivity implements FirebaseErrorDi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        inputHomeName = findViewById(R.id.activity_signin_input_homename_value);
-        inputUsername = findViewById(R.id.activity_signin_input_username_value);
+        inputEmail = findViewById(R.id.activity_signin_input_email_value);
         inputPassword = findViewById(R.id.activity_signin_input_password_value);
-        layoutHomeName = findViewById(R.id.activity_signin_input_homename);
-        layoutUsername = findViewById(R.id.activity_signin_input_username);
+        layoutEmail = findViewById(R.id.activity_signin_input_email);
         layoutPassword = findViewById(R.id.activity_signin_input_password);
 
         /*
@@ -52,22 +46,16 @@ public class SignInActivity extends AppCompatActivity implements FirebaseErrorDi
         è il textLayout, e per qualche motivo invocando getParent sul textInput NON viene restituito
         il textLayout.
          */
-        inputHomeName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) resetErrorMessage(layoutHomeName);
-            }
-        });
         inputPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (hasFocus) resetErrorMessage(layoutPassword);
             }
         });
-        inputUsername.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        inputEmail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) resetErrorMessage(layoutUsername);
+                if (hasFocus) resetErrorMessage(layoutEmail);
             }
         });
 
@@ -77,7 +65,7 @@ public class SignInActivity extends AppCompatActivity implements FirebaseErrorDi
             public void onClick(View v) {
                 KeyboardUtils.hideKeyboard(SignInActivity.this);
                 if (checkInput()) {
-                    retrieveEmailFromDb(inputHomeName.getText().toString(), inputUsername.getText().toString(), inputPassword.getText().toString());
+                    performSignIn(inputEmail.getText().toString(), inputPassword.getText().toString());
                 }
             }
         });
@@ -88,7 +76,7 @@ public class SignInActivity extends AppCompatActivity implements FirebaseErrorDi
         inputLayout.setErrorEnabled(false);
     }
 
-    private void retrieveEmailFromDb(final String homeName, final String username, final String password) {
+    /*private void retrieveEmailFromDb(final String username, final String password) {
         progress = ProgressDialog.show(
                 this,
                 getString(R.string.activity_signin_progress_title),
@@ -108,33 +96,38 @@ public class SignInActivity extends AppCompatActivity implements FirebaseErrorDi
                 } else {
                     // Se fallisco qui so che è il nome della casa o lo username ad essere errato
                     layoutHomeName.setError(getString(R.string.form_error_wrong_home_username));
-                    layoutUsername.setError(getString(R.string.form_error_wrong_home_username));
+                    layoutEmail.setError(getString(R.string.form_error_wrong_home_username));
                     progress.dismiss();
                 }
-                /*
+                *//*
                 NOTA: Non si può mettere QUI progress.dismiss() perché nell'if viene chiamato il
                 metodo performSignIn che contiene un'operazione asincrona. Se si mettesse qui
                 progress.dismiss() il dialog potrebbe chiudersi prima che l'operazione performSignIn
                 sia stata completata, e questo NON è quello che si vuole.
-                 */
+                 *//*
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                /*
+                *//*
                 onCancelled viene invocato solo se si verifica un errore a lato server oppure se
                 le regole di sicurezza impostate in Firebase non permettono l'operazione richiesta.
                 In questo caso perciò viene visualizzato un messaggio di errore generico, dato che
                 la situazione non può essere risolta dall'utente.
-                 */
+                 *//*
                 FirebaseErrorDialogFragment dialog = new FirebaseErrorDialogFragment();
                 progress.dismiss();
                 dialog.show(getSupportFragmentManager(), FirebaseErrorDialogFragment.TAG_FIREBASE_ERROR_DIALOG);
             }
         });
-    }
+    }*/
 
     private void performSignIn(String email, final String password) {
+        progress = ProgressDialog.show(
+                this,
+                getString(R.string.activity_signin_progress_title),
+                getString(R.string.activity_signin_progress_description), true);
+
         final FirebaseAuth auth = FirebaseAuth.getInstance();
         auth.signInWithEmailAndPassword(email, password)
                 .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
@@ -148,12 +141,11 @@ public class SignInActivity extends AppCompatActivity implements FirebaseErrorDi
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         /*
-                        Un fallimento in questo punto implica che l'utente ha inserito la password errata.
-                        Tener presente che questo è vero SOLO se Auth e DB su Firebase sono perfettamente
-                        allineati: in caso di discrepanze (se l'email recuperata dal DB non corrisponde
-                        a nessun account esistente su Auth) l'errore non è legato alla password.
+                        Un fallimento in questo punto implica che le credenziali inserite dall'utente
+                        non sono corrette.
                          */
-                        layoutPassword.setError(getString(R.string.form_error_incorrect_password));
+                        layoutEmail.setError(getString(R.string.form_error_incorrect_credentials));
+                        layoutPassword.setError(getString(R.string.form_error_incorrect_credentials));
                         progress.dismiss();
                     }
                 });
@@ -171,22 +163,17 @@ public class SignInActivity extends AppCompatActivity implements FirebaseErrorDi
     }
 
     private boolean checkInput() {
-        resetErrorMessage(layoutHomeName);
-        resetErrorMessage(layoutUsername);
+        resetErrorMessage(layoutEmail);
         resetErrorMessage(layoutPassword);
 
-        String homeNameValue = inputHomeName.getText().toString();
-        String usernameValue = inputUsername.getText().toString();
+        String emailValue = inputEmail.getText().toString();
         String passwordValue = inputPassword.getText().toString();
 
         boolean result = true;
-        // Parto controllando che tutti i campi siano compilati
-        if (homeNameValue.trim().length() == 0) {
-            layoutHomeName.setError(getString(R.string.form_error_missing_value));
-            result = false;
-        }
-        if (usernameValue.trim().length() == 0) {
-            layoutUsername.setError(getString(R.string.form_error_missing_value));
+
+        // Controllo che tutti i campi siano compilati
+        if (emailValue.trim().length() == 0) {
+            layoutEmail.setError(getString(R.string.form_error_missing_value));
             result = false;
         }
         if (passwordValue.trim().length() == 0) {
@@ -194,11 +181,17 @@ public class SignInActivity extends AppCompatActivity implements FirebaseErrorDi
             result = false;
         }
 
+        // Controllo che la mail inserita sia valida
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(emailValue).matches()) {
+            layoutEmail.setError(getString(R.string.form_error_incorrect_email));
+            result = false;
+        }
+
         return result;
     }
 
     private void moveToNextActivity() {
-        Intent i = new Intent(SignInActivity.this, MainActivity.class);
+        Intent i = new Intent(SignInActivity.this, HomeListActivity.class);
         startActivity(i);
         finish();
     }
