946438b5c7fbd6cbabb66ae3322a1c38494cb656
gianluca150 <pigic116@gmail.com>
2019-05-15 18:23:22 +0200

    Modificata gestione della deadline dei task Creata nuova classe DateUtils per poter gestire diversi formati della data; modificato salvataggio dei dati nel db in modo che le date vengano scritte in un formato 'neutro' ma che vengano poi convertite a runtime in modo da mostrarle all'utente nel formato specifico della lingua impostata sul dispositivo.

--- app/src/main/java/com/unison/appartment/activities/CreateTaskActivity.java
+++ app/src/main/java/com/unison/appartment/activities/CreateTaskActivity.java
@@ -1,5 +1,6 @@
 package com.unison.appartment.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -17,6 +19,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.unison.appartment.fragments.DatePickerFragment;
 import com.unison.appartment.R;
 import com.unison.appartment.model.Task;
+import com.unison.appartment.utils.DateUtils;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -28,14 +31,14 @@ import java.util.Locale;
  */
 public class CreateTaskActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
-    // Date formatter utilizzato per formattare la deadline
-    private DateFormat dateFormatter;
+    private final static String BUNDLE_KEY_DATE_STRING = "dateString";
 
     private Toolbar toolbar;
     private EditText inputName;
     private EditText inputDescription;
     private EditText inputPoints;
     private EditText inputDeadline;
+    private String deadlineDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,11 +76,6 @@ public class CreateTaskActivity extends AppCompatActivity implements DatePickerD
             }
         });
 
-        // Nota: dateFormatter non è static ed è inizializzato alla creazione dell'activity
-        // perché il cambio di Locale è proprio una delle modifiche che possono causare il
-        // riavvio dell'activity
-        this.dateFormatter = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault());
-
         /*
         Se lo schermo è stato ruotato mentre il date picker era aperto, l'activity è stata distrutta
         e ora sta venendo ricreata. Si vuole mantenere aperto lo stesso date picker, a cui però
@@ -90,11 +88,25 @@ public class CreateTaskActivity extends AppCompatActivity implements DatePickerD
         }
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putString(BUNDLE_KEY_DATE_STRING, deadlineDate);
+
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        deadlineDate = savedInstanceState.getString(BUNDLE_KEY_DATE_STRING);
+    }
+
     public void createTask() {
         Task newTask = new Task(
                 inputName.getText().toString(),
                 inputDescription.getText().toString(),
-                inputDeadline.getText().toString(),
+                deadlineDate, // La data viene salvata in un formato indipendente dalla lingua utilizzata nel device
                 Integer.valueOf(inputPoints.getText().toString())
         );
         Intent returnIntent = new Intent();
@@ -111,12 +123,16 @@ public class CreateTaskActivity extends AppCompatActivity implements DatePickerD
         // (non faccio niente perché Calendar.getInstance() mi restituisce già la data corrente)
 
         // Altrimenti, uso la data precedentemente selezionata come quella di default
-        if (inputDeadline.getText().length() != 0) {
+        if (deadlineDate != null) {
             try {
-                cal.setTime(dateFormatter.parse(inputDeadline.getText().toString()));
+                cal.setTime(DateUtils.parseDateWithStandardLocale(deadlineDate));
             }
             catch (ParseException e) {
-                throw new RuntimeException(e);
+                /*
+                Questa eccezione non dovrebbe mai verificarsi in quanto il parsing viene eseguito
+                con uno standard locale che è sempre uguale anche se cambia la lingua del device!
+                 */
+                Log.e(getClass().getCanonicalName(), e.getMessage());
             }
         }
 
@@ -131,6 +147,7 @@ public class CreateTaskActivity extends AppCompatActivity implements DatePickerD
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
         Calendar cal = Calendar.getInstance();
         cal.set(year, month, dayOfMonth);
-        inputDeadline.setText(dateFormatter.format(cal.getTime()));
+        inputDeadline.setText(DateUtils.formatDateWithCurrentDefaultLocale(cal.getTime()));
+        deadlineDate = DateUtils.formatDateWithStandardLocale(cal.getTime());
     }
 }
