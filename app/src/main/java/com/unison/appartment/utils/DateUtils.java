cdc8fa172d645af3394471af0a59d4a423c196f4
gianluca150 <pigic116@gmail.com>
2019-05-16 08:54:14 +0200

    Modificato formato standard delle date

--- app/src/main/java/com/unison/appartment/utils/DateUtils.java
+++ app/src/main/java/com/unison/appartment/utils/DateUtils.java
@@ -2,6 +2,7 @@ package com.unison.appartment.utils;
 
 import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
@@ -10,7 +11,8 @@ public class DateUtils {
     public final static Locale STANDARD_LOCALE = Locale.US;
 
     public static String formatDateWithStandardLocale(Date date) {
-        return formatDateWithSpecificLocale(date, STANDARD_LOCALE);
+        DateFormat iso8601 = new SimpleDateFormat("yyyy-MM-dd", STANDARD_LOCALE);
+        return iso8601.format(date);
     }
 
     public static String formatDateWithCurrentDefaultLocale(Date date) {
@@ -23,7 +25,8 @@ public class DateUtils {
     }
 
     public static Date parseDateWithStandardLocale(String dateString) throws ParseException {
-        return parseDateWithSpecificLocale(dateString, STANDARD_LOCALE);
+        DateFormat iso8601 = new SimpleDateFormat("yyyy-MM-dd", STANDARD_LOCALE);
+        return iso8601.parse(dateString);
     }
 
     public static Date parseDateWithCurrentDefaultLocale(String dateString) throws ParseException {
