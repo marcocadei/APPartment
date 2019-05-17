5bfcb7433db940122183a22ed5a1af3c1c4265d5
MarcoCadei <m.cadei010@studenti.unibs.it>
2019-05-17 21:29:55 +0200

    Implementato metodo di scrittura generale in DatabaseWriter

--- app/src/main/java/com/unison/appartment/database/DatabaseWriter.java
+++ app/src/main/java/com/unison/appartment/database/DatabaseWriter.java
@@ -1,7 +1,11 @@
 package com.unison.appartment.database;
 
 import com.unison.appartment.model.User;
+import java.util.Map;
 
 public interface DatabaseWriter {
+    // Metodo di scrittura generale
+    void write(Map<String, Object> childUpdates, final DatabaseWriterListener listener);
+
     void writeUser(final User newUser, final String uid, final DatabaseWriterListener listener);
 }
--- app/src/main/java/com/unison/appartment/database/FirebaseDatabaseWriter.java
+++ app/src/main/java/com/unison/appartment/database/FirebaseDatabaseWriter.java
@@ -7,26 +7,38 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.unison.appartment.MyApplication;
 import com.unison.appartment.R;
 import com.unison.appartment.model.User;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FirebaseDatabaseWriter implements DatabaseWriter {
-    public void writeUser(final User newUser, final String uid, final DatabaseWriterListener listener) {
-        // Scrittura dei dati relativi al nuovo utente nel database
-        String path = DatabaseConstants.USERS + DatabaseConstants.SEPARATOR + uid;
-        DatabaseReference dbRef = com.google.firebase.database.FirebaseDatabase.getInstance().getReference(path);
-        dbRef.setValue(newUser)
+    @Override
+    public void write(final Map<String, Object> childUpdates, final DatabaseWriterListener listener) {
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+        dbRef.updateChildren(childUpdates)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
                             listener.onWriteSuccess();
-                        } else {
+                        }
+                        else {
                             listener.onWriteFail(task.getException());
                         }
                     }
                 });
+    }
+
+    public void writeUser(final User newUser, final String uid, final DatabaseWriterListener listener) {
+        // Scrittura dei dati relativi al nuovo utente nel database
+        String path = DatabaseConstants.USERS + DatabaseConstants.SEPARATOR + uid;
+        HashMap<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put(path, newUser);
+        write(childUpdates, listener);
     }
 }
