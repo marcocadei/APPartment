a3cbaea715b9bfd8de40aea34796cc0624fa3265
MarcoCadei <m.cadei010@studenti.unibs.it>
2019-05-17 18:46:43 +0200

    Rinominati metodi di Auth

--- app/src/main/java/com/unison/appartment/database/Auth.java
+++ app/src/main/java/com/unison/appartment/database/Auth.java
@@ -3,9 +3,8 @@ package com.unison.appartment.database;
 import com.unison.appartment.model.User;
 
 public interface Auth {
-    void writeAuthInfo(final User newUser, final AuthListener listener);
+    void signUp(final User newUser, final AuthListener listener);
+    void signIn(final String email, final String password, final AuthListener listener);
 
     String getCurrentUserUid();
-
-    void performSignIn(final String email, final String password, final AuthListener listener);
 }
