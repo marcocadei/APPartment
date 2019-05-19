3d01a747b6db04dffe83dbbc3843250b30d86300
MarcoCadei <m.cadei010@studenti.unibs.it>
2019-05-19 19:29:27 +0200

    Aggiunti campi mancanti ad Appartment

--- app/src/main/java/com/unison/appartment/state/Appartment.java
+++ app/src/main/java/com/unison/appartment/state/Appartment.java
@@ -2,10 +2,12 @@ package com.unison.appartment.state;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-
 import com.google.gson.Gson;
 import com.unison.appartment.model.Home;
+import com.unison.appartment.model.HomeUser;
 import com.unison.appartment.model.User;
+import com.unison.appartment.model.UserHome;
+
 
 /**
  * Classe che rappresenta lo stato globale dell'applicazione
@@ -18,6 +20,8 @@ public class Appartment {
 
     private Home home;
     private User user;
+    private UserHome userHome;
+    private HomeUser homeUser;
 
     public void setHome(Home home) {
         SharedPreferences sp = MyApplication.getAppContext().getSharedPreferences(SharedPreferencesConstants.FILE_KEY, Context.MODE_PRIVATE);
@@ -37,6 +41,14 @@ public class Appartment {
         return gson.fromJson(json, Home.class);
     }
 
+    public void setUser(User user) {
+        SharedPreferences sp = MyApplication.getAppContext().getSharedPreferences(SharedPreferencesConstants.FILE_KEY, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(SharedPreferencesConstants.USER_KEY, new Gson().toJson(user));
+        editor.apply();
+        this.user = user;
+    }
+
     public User getUser() {
         if(user != null) {
             return user;
@@ -47,11 +59,39 @@ public class Appartment {
         return gson.fromJson(json, User.class);
     }
 
-    public void setUser(User user) {
+    public void setUserHome(UserHome userHome) {
         SharedPreferences sp = MyApplication.getAppContext().getSharedPreferences(SharedPreferencesConstants.FILE_KEY, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
-        editor.putString(SharedPreferencesConstants.USER_KEY, new Gson().toJson(user));
+        editor.putString(SharedPreferencesConstants.USERHOME_KEY, new Gson().toJson(userHome));
         editor.apply();
-        this.user = user;
+        this.userHome = userHome;
+    }
+
+    public UserHome getUserHome() {
+        if(userHome != null) {
+            return userHome;
+        }
+        SharedPreferences sp = MyApplication.getAppContext().getSharedPreferences(SharedPreferencesConstants.FILE_KEY, Context.MODE_PRIVATE);
+        String json = sp.getString(SharedPreferencesConstants.USERHOME_KEY, null);
+        Gson gson = new Gson();
+        return gson.fromJson(json, UserHome.class);
+    }
+
+    public void setHomeUser(HomeUser homeUser) {
+        SharedPreferences sp = MyApplication.getAppContext().getSharedPreferences(SharedPreferencesConstants.FILE_KEY, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(SharedPreferencesConstants.HOMEUSER_KEY, new Gson().toJson(homeUser));
+        editor.apply();
+        this.homeUser = homeUser;
+    }
+
+    public HomeUser getHomeUser() {
+        if(homeUser != null) {
+            return homeUser;
+        }
+        SharedPreferences sp = MyApplication.getAppContext().getSharedPreferences(SharedPreferencesConstants.FILE_KEY, Context.MODE_PRIVATE);
+        String json = sp.getString(SharedPreferencesConstants.HOMEUSER_KEY, null);
+        Gson gson = new Gson();
+        return gson.fromJson(json, HomeUser.class);
     }
 }
