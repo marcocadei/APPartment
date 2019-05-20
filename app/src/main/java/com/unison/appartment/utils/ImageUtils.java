9054517fc26a835d46dcd2527915db2326adbddf
MarcoCadei <m.cadei010@studenti.unibs.it>
2019-05-20 21:37:38 +0200

    Inserito metodo per ridimensionare le immagini Il metodo ridimensiona le immagini preservando l'aspect ratio

---
+++ app/src/main/java/com/unison/appartment/utils/ImageUtils.java
@@ -0,0 +1,30 @@
+package com.unison.appartment.utils;
+
+import android.graphics.Bitmap;
+
+public class ImageUtils {
+
+    public final static int MAX_WIDTH = 680;
+    public final static int MAX_HEIGHT = 680;
+
+    public static Bitmap resize(Bitmap image, int maxWidth, int maxHeight) {
+        if (maxHeight > 0 && maxWidth > 0) {
+            int width = image.getWidth();
+            int height = image.getHeight();
+            float ratioBitmap = (float) width / (float) height;
+            float ratioMax = (float) maxWidth / (float) maxHeight;
+
+            int finalWidth = maxWidth;
+            int finalHeight = maxHeight;
+            if (ratioMax > ratioBitmap) {
+                finalWidth = (int) ((float)maxHeight * ratioBitmap);
+            } else {
+                finalHeight = (int) ((float)maxWidth / ratioBitmap);
+            }
+            image = Bitmap.createScaledBitmap(image, finalWidth, finalHeight, true);
+            return image;
+        } else {
+            return image;
+        }
+    }
+}
