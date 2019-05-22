2cbce8583b751ed7bb33ef6f02db2871d9f05497
gianluca150 <pigic116@gmail.com>
2019-05-22 10:27:45 +0200

    Reinserito un TODO (per ricordarsi che alcuni campi sono ancora da togliere)

--- app/src/main/java/com/unison/appartment/fragments/TodoListFragment.java
+++ app/src/main/java/com/unison/appartment/fragments/TodoListFragment.java
@@ -29,7 +29,9 @@ import java.util.List;
  */
 public class TodoListFragment extends Fragment {
 
+    // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
+    // TODO: Customize parameters
     private int mColumnCount = 1;
 
     private TodoTaskViewModel viewModel;
