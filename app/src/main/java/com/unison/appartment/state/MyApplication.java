c2cfa7787cb732fd655fa7186a3588d70ff55e3b
MarcoCadei <m.cadei010@studenti.unibs.it>
2019-05-22 21:34:38 +0200

    "Abilitato le capacità offline" così come scritto nella guida ufficiale. Sembra che non ci sia nessuna differenza, forse conviene indagare

--- app/src/main/java/com/unison/appartment/state/MyApplication.java
+++ app/src/main/java/com/unison/appartment/state/MyApplication.java
@@ -3,12 +3,16 @@ package com.unison.appartment.state;
 import android.app.Application;
 import android.content.Context;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MyApplication extends Application {
 
     private static Context context;
 
     public void onCreate() {
         super.onCreate();
+        // TODO vedere se va tolto o qualche miglioramento lo dà lo stesso
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         MyApplication.context = getApplicationContext();
     }
 
