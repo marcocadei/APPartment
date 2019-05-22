7795fbbe0444c47eb6c09c623ad4beb8976cf6c7
gianluca150 <pigic116@gmail.com>
2019-05-22 11:25:02 +0200

    Ristrutturata CreateRewardActivity come sottoclasse di FormActivity Introdotti controlli sui campi della form all'interno del metodo checkInput.

--- app/src/main/java/com/unison/appartment/activities/CreateRewardActivity.java
+++ app/src/main/java/com/unison/appartment/activities/CreateRewardActivity.java
@@ -10,20 +10,24 @@ import android.view.View;
 import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputLayout;
 import com.unison.appartment.R;
-import com.unison.appartment.fragments.RewardsFragment;
 import com.unison.appartment.model.Reward;
+import com.unison.appartment.utils.KeyboardUtils;
 
 /**
  * Classe che rappresenta l'Activity per creare un nuovo Reward
  */
-public class CreateRewardActivity extends AppCompatActivity {
+public class CreateRewardActivity extends FormActivity {
 
     public final static String EXTRA_NEW_REWARD = "newReward";
 
     private EditText inputName;
     private EditText inputDescription;
     private EditText inputPoints;
+    private TextInputLayout layoutName;
+    private TextInputLayout layoutDescription;
+    private TextInputLayout layoutPoints;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +38,7 @@ public class CreateRewardActivity extends AppCompatActivity {
         Impostazione del comportamento della freccia presente sulla toolbar
         (alla pressione l'activity viene terminata).
          */
-        Toolbar toolbar;
-        toolbar = findViewById(R.id.activity_create_reward_toolbar);
+        Toolbar toolbar = findViewById(R.id.activity_create_reward_toolbar);
         setSupportActionBar(toolbar);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,25 +47,82 @@ public class CreateRewardActivity extends AppCompatActivity {
             }
         });
 
+        // Modifica dell'activity di destinazione a cui andare quando si chiude il dialog di errore
+        this.errorDialogDestinationActivity = MainActivity.class;
+
         inputName = findViewById(R.id.activity_create_reward_input_name_value);
         inputDescription = findViewById(R.id.activity_create_reward_input_description_value);
         inputPoints = findViewById(R.id.activity_create_reward_input_points_value);
+        layoutName = findViewById(R.id.activity_create_reward_input_name);
+        layoutDescription = findViewById(R.id.activity_create_reward_input_description);
+        layoutPoints = findViewById(R.id.activity_create_reward_input_points);
+
+        inputName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) resetErrorMessage(layoutName);
+            }
+        });
+        inputDescription.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) resetErrorMessage(layoutDescription);
+            }
+        });
+        inputPoints.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) resetErrorMessage(layoutPoints);
+            }
+        });
 
         FloatingActionButton floatConfirm = findViewById(R.id.activity_create_reward_float_confirm);
         floatConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createReward();
+                KeyboardUtils.hideKeyboard(CreateRewardActivity.this);
+                if (checkInput()) {
+                    createReward();
+                }
             }
         });
+    }
+
+    @Override
+    protected boolean checkInput() {
+        resetErrorMessage(layoutName);
+        resetErrorMessage(layoutDescription);
+        resetErrorMessage(layoutPoints);
+
+        inputName.setText(inputName.getText().toString().trim());
+
+        String nameValue = inputName.getText().toString();
+        String descriptionValue = inputDescription.getText().toString();
+        String pointsValue = inputPoints.getText().toString();
+
+        boolean result = true;
+
+        // Controllo che i campi non siano stati lasciati vuoti
+        if (nameValue.trim().length() == 0) {
+            layoutName.setError(getString(R.string.form_error_missing_value));
+            result = false;
+        }
+        if (descriptionValue.trim().length() == 0) {
+            layoutDescription.setError(getString(R.string.form_error_missing_value));
+            result = false;
+        }
+        if (pointsValue.trim().length() == 0) {
+            layoutPoints.setError(getString(R.string.form_error_missing_value));
+            result = false;
+        }
+
+        return result;
     }
 
     /**
      * Metodo per creare un nuovo Reward
      */
     public void createReward() {
-        // FIXME modificare la struttura del metodo in modo che sia in linea con le altre activity
-
         Reward reward = new Reward(
                 inputName.getText().toString(),
                 inputDescription.getText().toString(),
