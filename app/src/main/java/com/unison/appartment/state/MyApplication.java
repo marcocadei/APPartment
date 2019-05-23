9f38139bdeab5c395fbe6404aa7a65639454cb90
MarcoCadei <m.cadei010@studenti.unibs.it>
2019-05-23 21:18:05 +0200

    Aggiunto commento che spiega cosa fa la setPersistenceEnabled di firebase

--- app/src/main/java/com/unison/appartment/state/MyApplication.java
+++ app/src/main/java/com/unison/appartment/state/MyApplication.java
@@ -12,6 +12,14 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         // TODO vedere se va tolto o qualche miglioramento lo dà lo stesso
+        /*
+        NB
+        Mantenere questa riga di codice abilita questa funzionalità: se si apre l'app, e si accede
+        con un utente, dopodiché si chiude l'app dalla schermata del profilo utente, si spegne la
+        rete e la si riapre allora l'app mostrerà comunque la lista delle case dell'utente.
+        Senza questa riga alla chiusura dell'app si perdono i dati e se la si riapre senza rete
+        si continua a vedere la progress bar.
+         */
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         MyApplication.context = getApplicationContext();
     }
