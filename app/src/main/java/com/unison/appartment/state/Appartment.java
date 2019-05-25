319df3fc19153fb5afe352f2a9e0999051d02c4d
MarcoCadei <m.cadei010@studenti.unibs.it>
2019-05-25 10:32:32 +0200

    Aggiunto aggiornamento continuo di Home

--- app/src/main/java/com/unison/appartment/state/Appartment.java
+++ app/src/main/java/com/unison/appartment/state/Appartment.java
@@ -5,7 +5,12 @@ import android.content.SharedPreferences;
 
 import androidx.annotation.NonNull;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
+import com.unison.appartment.database.DatabaseConstants;
 import com.unison.appartment.model.Home;
 import com.unison.appartment.model.HomeUser;
 import com.unison.appartment.model.User;
@@ -47,6 +52,28 @@ public class Appartment {
     public void setHome(@NonNull Home home) {
         setSharedPreferencesValue(SharedPreferencesConstants.HOME_KEY, new Gson().toJson(home));
         this.home = home;
+        /*
+        È necessario mantenere l'oggetto casa continuamente aggiornato perché il "name" e il "conversionFactor"
+        sono utilizzati all'interno della MainActivity, ma possono essere cambiati da un altro utente
+         */
+        DatabaseReference dbRef = com.google.firebase.database.FirebaseDatabase.getInstance().getReference(DatabaseConstants.HOMES + DatabaseConstants.SEPARATOR + home.getName());
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    setHomeFB(dataSnapshot.getValue(Home.class));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void setHomeFB(Home home) {
+        this.home = home;
     }
 
     public void resetHome() {
@@ -64,6 +91,14 @@ public class Appartment {
     public void setUser(@NonNull User user) {
         setSharedPreferencesValue(SharedPreferencesConstants.USER_KEY, new Gson().toJson(user));
         this.user = user;
+        /*
+        NB
+        Per l'utente non ha senso tenere i dati aggiornati con firebase perché solo lui può modificare i
+        propri dati andando nella UserProfileActivity, ma se ci va e li modifica allora poi quando rientra
+        nella MainActivity avremo già l'oggetto aggiornato.
+        Per gli altri oggetti invece è necessario il continuo aggiornamento perché possono essere modificati
+        da più fonti.
+         */
     }
 
     public void resetUser() {
