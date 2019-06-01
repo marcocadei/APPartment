556c50d701878638a6c09111f4a24a940c527051
Marco <m.cadei010@studenti.unibs.it>
2019-06-01 18:10:32 +0200

    Completato prima versione del layout della CompletedTaskDetailActivity

--- app/src/main/java/com/unison/appartment/CompletedTaskDetailActivity.java
+++ app/src/main/java/com/unison/appartment/CompletedTaskDetailActivity.java
@@ -1,8 +1,10 @@
 package com.unison.appartment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class CompletedTaskDetailActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class CompletedTaskDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_completed_task_detail);
+
+        /*
+        Impostazione del comportamento della freccia presente sulla toolbar
+        (alla pressione l'activity viene terminata).
+         */
+        Toolbar toolbar = findViewById(R.id.activity_completed_task_detail_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
