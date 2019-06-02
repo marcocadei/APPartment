17ad5f26ae0fca389890e78a0f24bd834fd7f911
Marco <m.cadei010@studenti.unibs.it>
2019-06-02 15:57:46 +0200

    Aggiunto HomeUserViewModel

---
+++ app/src/main/java/com/unison/appartment/viewmodel/HomeUserViewModel.java
@@ -0,0 +1,24 @@
+package com.unison.appartment.viewmodel;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.unison.appartment.model.HomeUser;
+import com.unison.appartment.repository.HomeUserRepository;
+
+import java.util.List;
+
+public class HomeUserViewModel extends ViewModel {
+
+    private HomeUserRepository repository;
+
+    public HomeUserViewModel() {
+        repository = new HomeUserRepository();
+    }
+
+    @NonNull
+    public LiveData<List<HomeUser>> getHomeUserLiveData() {
+        return repository.getHomeUserLiveData();
+    }
+}
