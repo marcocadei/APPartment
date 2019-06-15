98bda576398b047ca1c6584aacec125e262fdc3e
Marco <m.cadei010@studenti.unibs.it>
2019-06-15 14:51:47 +0200

    Rimossi improt inutili

--- app/src/main/java/com/unison/appartment/activities/ActivityWithDialogs.java
+++ app/src/main/java/com/unison/appartment/activities/ActivityWithDialogs.java
@@ -3,13 +3,7 @@ package com.unison.appartment.activities;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.util.Log;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import com.google.android.material.textfield.TextInputLayout;
 import com.unison.appartment.fragments.FirebaseErrorDialogFragment;
 import com.unison.appartment.fragments.FirebaseProgressDialogFragment;
 import com.unison.appartment.fragments.NetworkErrorDialogFragment;
